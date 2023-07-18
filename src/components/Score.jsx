c46780aa00ae45aa1dac73ff71ee475784dd7b12
Damola Olusakin <olusakinadedamola@gmail.com>
2023-07-18 15:00:04 -0400

    created a score component

---
+++ src/components/Score.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Score() {
+  return (
+    <div>THis is a score</div>
+  )
+}
+
+export default Score
--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import Score from "./Score";
 
-function Student({student}) {
+function Student({ student }) {
   return (
     <div>
- this is a student
+      this is a student
+      <Score />
     </div>
-  )
+  );
 }
 
-export default Student
+export default Student;
